939e0f81fd4e714d8a3fece319bbc6fb3b3f779a
Gert Peter Rue Brigsted <manisto@manisto.dk>
2018-11-12 11:24:09 +0100

    Started adding edit component.

---
+++ app/recipe-edit.component.ts
@@ -0,0 +1,11 @@
+class RecipeEditController {
+
+}
+
+export let RecipeEditComponent: ng.IComponentOptions = {
+    controller: RecipeEditController,
+    templateUrl: "/app/recipe-edit.html",
+    bindings: {
+        recipe: "<"
+    }
+};
--- app/recipes.module.ts
+++ app/recipes.module.ts
@@ -2,6 +2,7 @@ declare let angular: typeof import("angular");
 
 import {AppComponent} from "./app.component.js";
 import {RecipeViewComponent} from "./recipe-view.component.js";
+import {RecipeEditComponent} from "./recipe-edit.component.js";
 import {RecipeHtmlService} from "./recipe-html.service.js";
 import {markdownFilter} from "./markdown.filter.js";
 
@@ -9,5 +10,6 @@ let recipesModule = angular.module("recipes", ["ngSanitize"]);
 
 recipesModule.component("app", AppComponent);
 recipesModule.component("recipeView", RecipeViewComponent);
+recipesModule.component("recipeEdit", RecipeEditComponent);
 recipesModule.service("recipeHtmlService", RecipeHtmlService);
 recipesModule.filter("markdown", markdownFilter);
