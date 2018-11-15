25cfbae960728bf54744a1f7853a2fb661a68a47
Gert Peter Rue Brigsted <manisto@manisto.dk>
2018-11-15 10:27:01 +0100

    Added file-input directive.

---
+++ app/file-input.directive.ts
@@ -0,0 +1,29 @@
+interface FileInputScope extends ng.IRootScopeService {
+    onFilesSelected: ({files: FileList}) => void;
+}
+
+export let fileInput: ng.IDirectiveFactory = () => {
+    function link(scope: FileInputScope, element: JQLite) {
+        let button: JQLite = element.find("button");
+        let input: JQLite = element.find("input");
+
+        button.on("click", () => {
+            input[0].click();
+        });
+
+        input.on("change", (event: Event) => {
+            scope.$apply(() => {
+                let files: FileList = (event.target as HTMLInputElement).files;
+                scope.onFilesSelected({ files });
+            });
+        });
+    }
+
+    return {
+        link,
+        templateUrl: "/app/file-input.html",
+        scope: {
+            onFilesSelected: "&"
+        }
+    };
+};
--- app/recipes.module.ts
+++ app/recipes.module.ts
@@ -5,7 +5,7 @@ import {RecipeViewComponent} from "./recipe-view.component.js";
 import {RecipeEditComponent} from "./recipe-edit.component.js";
 import {RecipeHtmlService} from "./recipe-html.service.js";
 import {markdownFilter} from "./markdown.filter.js";
-import {onFilesSelected} from "./on-files-selected.directive.js";
+import {fileInput} from "./file-input.directive.js"
 
 let recipesModule = angular.module("recipes", ["ngSanitize", "ui.bootstrap"]);
 
@@ -14,7 +14,7 @@ recipesModule.component("recipeView", RecipeViewComponent);
 recipesModule.component("recipeEdit", RecipeEditComponent);
 recipesModule.service("recipeHtmlService", RecipeHtmlService);
 recipesModule.filter("markdown", markdownFilter);
-recipesModule.directive("onFilesSelected", onFilesSelected);
+recipesModule.directive("fileInput", fileInput);
 
 recipesModule.config(["$compileProvider", function($compileProvider: ng.ICompileProvider) {
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(blob):/);
